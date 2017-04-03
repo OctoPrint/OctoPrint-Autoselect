97e6403d9f5ac9f9ca38484db4c3125b3dca8190
Gina Häußge <gina@octoprint.org>
2017-04-03 19:01:28 +0200

    Use new FileAdded event if available
    That will allow us to files added by other means than the
    file upload of the REST API (e.g. the watched folder or
    the slicing process).
    
    Solves #1 and #3

--- octoprint_autoselect/__init__.py
+++ octoprint_autoselect/__init__.py
@@ -2,28 +2,46 @@
 from __future__ import absolute_import
 
 import octoprint.plugin
+import octoprint.events
 from octoprint.filemanager.destinations import FileDestinations
 
 class AutoselectPlugin(octoprint.plugin.EventHandlerPlugin):
+	def __init__(self):
+		self._event = None
+		if hasattr(octoprint.events.Events, "FILE_ADDED"):
+			# OctoPrint >= 1.3.3: we can monitor the FileAdded event as triggered
+			# by the file manager
+			self._event = octoprint.events.Events.FILE_ADDED
+			def to_storage_and_name(payload):
+				return payload["storage"], payload["path"]
+		else:
+			# OctoPrint < 1.3.3: we can only react to the upload event, files added
+			# through e.g. the watched folder or other means than the REST API are
+			# left out. See also #1
+			self._event = octoprint.events.Events.UPLOAD
+			def to_storage_and_name(payload):
+				return payload["target"], payload["path"]
+
+		self._to_storage_and_name = to_storage_and_name
+
 	def on_event(self, event, payload):
-		if event != "Upload":
+		if event != self._event:
 			return
 
 		if not self._printer.is_ready():
 			self._logger.debug("Printer is not ready, not autoselecting uploaded file")
 			return
 
-		filename = payload["file"]
-		target = payload["target"]
+		storage, filename = self._to_storage_and_name(payload)
 
-		if target == FileDestinations.SDCARD:
+		if storage == FileDestinations.SDCARD:
 			path = filename
 			sd = True
 		else:
-			path = self._file_manager.path_on_disk(target, filename)
+			path = self._file_manager.path_on_disk(storage, filename)
 			sd = False
 
-		self._logger.info("Selecting {} on {} that was just uploaded".format(filename, target))
+		self._logger.info("Selecting {} on {} that was just uploaded".format(filename, storage))
 		self._printer.select_file(path, sd, False)
 
 __plugin_name__ = "Autoselect Plugin"
--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ plugin_package = "octoprint_autoselect"
 plugin_name = "OctoPrint-Autoselect"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.0"
+plugin_version = "0.2.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Automatically selects freshly uploaded files for printing if no print job is currently active."""
+plugin_description = """Automatically selects freshly added files for printing if no print job is currently active."""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Gina Häußge"
